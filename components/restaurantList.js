345e243859c01446629c1e7cdc30e17c1c6560dd
YannGueguen1 <yanngueguen2015@u.northwestern.edu>
2022-04-16 20:22:52 -0500

    modify dishes.js and restaurants.js

--- components/restaurantList.js
+++ components/restaurantList.js
@@ -1,9 +1,8 @@
-import {gql,useQuery} from '@apollo/client';
-import Dishes from "./dishes"
-import {useContext, useState} from 'react';
+import { gql, useQuery } from "@apollo/client";
+import Dishes from "./dishes";
+import { useContext, useState } from "react";
 
-
-import AppContext from "./context"
+import AppContext from "./context";
 import {
   Button,
   Card,
@@ -13,12 +12,13 @@ import {
   CardTitle,
   Container,
   Row,
-  Col} from "reactstrap";
+  Col,
+} from "reactstrap";
 
-function RestaurantList(props){
-  const[restaurantID, setRestaurantID] = useState(0)
-  const {cart } = useContext(AppContext);
-  const [state, setState] = useState(cart)
+function RestaurantList(props) {
+  const [restaurantID, setRestaurantID] = useState(0);
+  const { cart } = useContext(AppContext);
+  const [state, setState] = useState(cart);
   const GET_RESTAURANTS = gql`
     query {
       restaurants {
@@ -31,63 +31,58 @@ function RestaurantList(props){
       }
     }
   `;
-  const { loading, error, data } = useQuery(GET_RESTAURANTS)
+  const { loading, error, data } = useQuery(GET_RESTAURANTS);
   if (loading) return <p>Loading...</p>;
   if (error) return <p>ERROR</p>;
   if (!data) return <p>Not found</p>;
-  console.log(`Query Data: ${data.restaurants}`)
+  console.log(`Query Data: ${data.restaurants}`);
 
+  let searchQuery = data.restaurants.filter((res) => {
+    return res.name.toLowerCase().includes(props.search);
+  });
 
-let searchQuery = data.restaurants.filter((res) =>{
-    return res.name.toLowerCase().includes(props.search)
-  })
+  let restId = searchQuery[0].id;
 
-let restId = searchQuery[0].id
- 
-// definet renderer for Dishes
+  // definet renderer for Dishes
   const renderDishes = (restaurantID) => {
-    return (<Dishes restId={restaurantID}> </Dishes>)
+    return <Dishes restId={restaurantID}> </Dishes>;
   };
-if(searchQuery.length > 0){
-  const restList = searchQuery.map((res) => (
-    <Col xs="6" sm="4" key={res.id}>
-      <Card style={{ margin: "0 0.5rem 20px 0.5rem" }}>
-        <CardImg
-          top={true}
-          style={{ height: 200 }}
-          src={ process.env.NODE_ENV === "production"
-          ? res.image.url : `${process.env.NEXT_PUBLIC_API_URL}${res.image.url}`}
-        />
-        <CardBody>
-          <CardText style={{"height":"72px"}}>{res.description}</CardText>
-        </CardBody>
-        <div className="card-footer">
-        
-        <Button color="info" onClick={()=> setRestaurantID(res.id)}>{res.name}</Button>
-         
-        </div>
-      </Card>
-    </Col>
-  ))
+  if (searchQuery.length > 0) {
+    const restList = searchQuery.map((res) => (
+      <Col xs="6" sm="4" key={res.id}>
+        <Card style={{ margin: "0 0.5rem 20px 0.5rem" }}>
+          <CardImg
+            top={true}
+            style={{ height: 200 }}
+            src={
+              process.env.NODE_ENV === "production"
+                ? res.image.url
+                : `${process.env.NEXT_PUBLIC_API_URL}${res.image.url}`
+            }
+          />
+          <CardBody>
+            <CardText style={{ height: "72px" }}>{res.description}</CardText>
+          </CardBody>
+          <div className="card-footer">
+            <Button color="info" onClick={() => setRestaurantID(res.id)}>
+              {res.name}
+            </Button>
+          </div>
+        </Card>
+      </Col>
+    ));
 
-  return(
+    return (
+      <Container>
+        <h2>List of restaurants:</h2>
+        <Row xs="3">{restList}</Row>
 
-    <Container>
-      <h2>List of restaurants:</h2>
-    <Row xs='3'>
-      {restList}
-    </Row>
-  
-    <Row xs='3'>
-      <h2>List of dishes:</h2>
-    {renderDishes(restaurantID)}
-    </Row>
- 
-    </Container>
- 
-  )
-} else {
-  return <h1> No Restaurants Found</h1>
+        {restID> 0 && <h2>List of dishes:</h2>}
+        <Row xs="3">{renderDishes(restaurantID)}</Row>
+      </Container>
+    );
+  } else {
+    return <h1> No Restaurants Found</h1>;
+  }
 }
-}
-   export default RestaurantList
+export default RestaurantList;
